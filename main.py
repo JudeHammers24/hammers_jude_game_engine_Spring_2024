fe30b117df4a079ef14ec474a8de3826f6c68b5e
JudeHammers24 <142941815+JudeHammers24@users.noreply.github.com>
2024-03-07 14:39:15 -0800

    added start screen - fifth push

--- main.py
+++ main.py
@@ -100,6 +100,14 @@ class Game:
             for y in range(0, HEIGHT, TILESIZE):
                 pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
 
+    def draw_text(self, surface, text, size, color, x, y):
+         font_name = pg.font.match_font('arial')
+         font = pg.font.Font(font_name, size)
+         text_surface = font.render(text, True, color)
+         text_rect = text_surface.get_rect()
+         text_rect.topleft = (x,y)
+         surface.blit(text_surface, text_rect)
+
     def draw(self):
             self.screen.fill(BGCOLOR)
             self.draw_grid()
@@ -123,8 +131,26 @@ class Game:
                 #     if event.key == pg.K_ESCAPE:
                 #         self.quit()
                     
+    def show_start_screen(self):
+         self.screen.fill(BGCOLOR)
+         self.draw_text(self.screen, "This is the start screen", 24, WHITE, WIDTH/2 - 32, 2)
+         pg.display.flip()
+         self.wait_for_key()
+
+    def wait_for_key(self):
+         waiting =  True
+         while waiting:
+              self.clock.tick(FPS)
+              for event in pg.event.get():
+                    if event.type == pg.QUIT:
+                        waiting = False
+                        self.quit()
+                    if event.type == pg.KEYUP:
+                        waiting = False
+                   
+
 g = Game()
-# g.show_start_screen()
+g.show_start_screen()
 while True:
     g.new()
     g.run()
